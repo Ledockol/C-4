94d4ccb7ef365e87834743cd57da6092a88fd9f3
led Ice <Andrey-ssk98@yandex.ru>
2022-09-16 02:35:10 +0300

    ex27

---
+++ ex27/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+Console.WriteLine("Введите массив");
+
+int[] CreateArray()
+{
+    Random rnd = new Random();
+    int[] array = new int[8];
+
+    for (int i = 0; i < array.Length; i++ )
+    {
+        array[i] = rnd.Next(0, 10);
+    }
+    return array;
+} 
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"{array[i]}\t");
+    }
+}
+
+int[] arr = CreateArray();
+PrintArray(arr);
