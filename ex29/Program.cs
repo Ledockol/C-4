b0cbc3507a6b1e66ffeef2c570a4b4c484e0da58
led Ice <Andrey-ssk98@yandex.ru>
2022-09-16 02:36:44 +0300

    ex29

---
+++ ex29/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе
+Console.Write("Введи число: ");
+int Num = Convert.ToInt32(Console.ReadLine());
+int Sum = 0;
+while (Num > 0)
+{
+int NumSecond = Num % 10;
+Num = Num / 10;
+Sum = Sum + NumSecond;
+}
+Console.WriteLine($"сумма цифр в числе равна:{Sum}");
+
+ 
