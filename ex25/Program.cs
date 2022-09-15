198a2caf6a3aba53db5eac772c138fe71633f907
led Ice <Andrey-ssk98@yandex.ru>
2022-09-16 02:32:48 +0300

    ex25

---
+++ ex25/Program.cs
@@ -0,0 +1,20 @@
+﻿//  Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+Console.WriteLine("Введите число");
+int Num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральную степень");
+int NumDegree = Convert.ToInt32(Console.ReadLine());
+
+GetDegree(Num, NumDegree);
+
+void GetDegree(int A, int B)
+{
+        double result = Math.Pow(A, B);
+    if(B >= 0)
+    {
+        Console.WriteLine($"{result}");
+    }
+    else 
+    {
+        Console.WriteLine("Степень не натуральое число");
+    }
+}
